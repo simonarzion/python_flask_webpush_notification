ff4b5b8d177e24386d3a911d172277f5f7d67bef
simonarzion <simon.villaverde@arzion.com>
2022-10-14 14:28:44 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import json, os
+
+from flask import request, Response, jsonify, render_template, Flask
+from pywebpush import webpush
+
+app = Flask(__name__)
+
+PRIVATE_KEY_PATH = os.path.join(os.getcwd(), "private_key.txt")
+PUBLIC_KEY_PATH = os.path.join(os.getcwd(), "public_key.txt")
+
+VAPID_PRIVATE_KEY = None
+VAPID_PUBLIC_KEY = None
+
+with open(PRIVATE_KEY_PATH, "r+", encoding='utf-8') as file:
+    VAPID_PRIVATE_KEY = file.readline().strip("\n")
+with open(PUBLIC_KEY_PATH, "r+", encoding='utf-8') as file:
+    VAPID_PUBLIC_KEY = file.read().strip("\n")
+
+# more info about vapid claims: https://github.com/web-push-libs/vapid/tree/main/python
+
+
+def send_web_push(subscription_information, message_body):
+    aud_url = '/'.join(subscription_information['endpoint'].split('/')[0:3])
+
+    return webpush(subscription_info=subscription_information,
+                   data=message_body,
+                   vapid_private_key=VAPID_PRIVATE_KEY,
+                   vapid_claims={
+                       "sub": "mailto:develop@raturi.in",
+                       "aud": aud_url
+                   })
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/notifications/subscription", methods=["GET"])
+def subscription():
+    """
+        GET returns vapid public key which clients uses to send around push notification
+    """
+
+    return Response(response=json.dumps({"public_key": VAPID_PUBLIC_KEY}),
+                    headers={"Access-Control-Allow-Origin": "*"},
+                    content_type="application/json")
+
+
+@app.route("/notifications/push", methods=['POST'])
+def push_v1():
+    """
+        POST gets the payload for push notificacion and sends it
+    """
+    try:
+        if not request.json or not request.json.get('subscription_info'):
+            return Response(status=400)
+
+        subscription_json = request.json.get('subscription_info')
+        subscription_info = json.loads(subscription_json)
+        send_web_push(subscription_info, "Push Test v1")
+        return Response(status=200)
+
+    except Exception as e:
+        return jsonify({'failed': str(e)})
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/sw.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log("[Service Worker]:", event.data);
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: `"${event.data.text()}"`,
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerPublicKey = localStorage.getItem('applicationServerPublicKey');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
+});
